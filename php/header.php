586e15f6d7343357964141f590077e83bcf13163
GeO0rGe3 <georgetimjab@icloud.com>
2022-12-21 22:21:59 +0300

    Initial commit

---
+++ php/header.php
@@ -0,0 +1,20 @@
+<?php
+    session_start();
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../css/style.css">
+</head>
+<header>
+    <div class="header">
+            <img src="../img/logo.png" alt="" srcset="">
+            <div>
+                <p>IT-КУБ | ЛИЧНЫЙ КАБИНЕТ</p>
+            </div>        
+    </div>
+</header>
---
+++ php/main.php
@@ -0,0 +1,22 @@
+<?php
+    require_once('header.php');
+?>
+<title>АВТОРИЗАЦИЯ</title>
+<div class="main">
+    <div>
+        <h1>АВТОРИЗАЦИЯ</h2>
+        <div>
+            <p>Логин:</p>
+            <input type="text" placeholder="Логин">
+        </div>
+        <div>
+            <p>Пароль:</p>
+            <input type="text" placeholder="Пароль">
+        </div>
+        <div>
+            <input type="submit" value="Зарегистрироваться">
+        </div>
+    </div>
+</div>
+
+
